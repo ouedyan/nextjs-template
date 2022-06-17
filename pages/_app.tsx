10ecd4b3d5521747e84e72b37141a261d6d00629
ouedyan <yanouedraogo5@gmail.com>
2022-06-17 15:40:04 +0300

    chore: updated dependencies chore: updated next js and remove ALink wrapper component (no longer necessary) feat: replaced old seo system with next-seo package feat: added example favicons feat: added example SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,45 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <>
+      <DefaultSeo
+        title="Next js React Web Project Template"
+        defaultTitle="Next js React Web Project Template"
+        titleTemplate="%s | Next js React Web Project Template"
+        description="Next js React Web Project Template (Typescript, Sass, Tailwind Css, SVGR, Prettier , SEO and On-Demand ISR pre-configs and other generally used features and tools in production.)"
+        canonical={`https://example.com${
+          router.asPath === "/" ? "" : router.asPath
+        }`}
+        openGraph={{
+          type: "website",
+          url: `https://example.com${
+            router.asPath === "/" ? "" : router.asPath
+          }`,
+          locale: "en",
+          title: "Next js React Web Project Template",
+          description:
+            "Next js React Web Project Template (Typescript, Sass, Tailwind Css, SVGR, Prettier , SEO and On-Demand ISR pre-configs and other generally used features and tools in production.)",
+          site_name: "Next js React Web Project Template",
+        }}
+        mobileAlternate={{
+          media: "only screen and (max-width: 640px)",
+          href: "https://example.com",
+        }}
+        twitter={{
+          handle: "@example",
+          site: "@example",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
