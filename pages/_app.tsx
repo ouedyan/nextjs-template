69ed5379e49f028e5dfee71582ddf891151772de
ouedyan <yanouedraogo5@gmail.com>
2022-06-19 07:03:41 +0300

    fix: some updates to _app's DefaultSeo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,10 @@ import { useRouter } from "next/router";
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
+  //TODO Change this SEO Defaults
   return (
     <>
       <DefaultSeo
-        title="Next js React Web Project Template"
         defaultTitle="Next js React Web Project Template"
         titleTemplate="%s | Next js React Web Project Template"
         description="Next js React Web Project Template (Typescript, Sass, Tailwind Css, SVGR, Prettier , SEO and On-Demand ISR pre-configs and other generally used features and tools in production.)"
@@ -29,7 +29,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
         mobileAlternate={{
           media: "only screen and (max-width: 640px)",
-          href: "https://example.com",
+          href: `https://example.com${
+            router.asPath === "/" ? "" : router.asPath
+          }`,
         }}
         twitter={{
           handle: "@example",
